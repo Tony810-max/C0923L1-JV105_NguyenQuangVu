b409a09ce6d4279c76df19a8369136e932b5ded7
quang vu <quangvu37781@gmail.com>
2023-11-03 17:31:07 +0700

    up second

--- 11_While-for/bai1/index.js
+++ 11_While-for/bai1/index.js
@@ -5,7 +5,7 @@ for (let i =1; i <= 10; i++){
     result += "<tr>"
     for (let j =1; j <=10;j++){
         let nhan = i *j;
-        result += "<td>" + i+ "x" + j+ "="+ nhan+ "</td>"
+        result += "<td>" + j+ "x" + i+ "="+ nhan+ "</td>"
     }
     result +="</tr>"
 }   

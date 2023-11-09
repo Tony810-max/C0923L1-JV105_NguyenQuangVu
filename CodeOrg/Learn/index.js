d2bb73f848e3881f3d5021a0aadf736afb800a7b
quang vu <quangvu37781@gmail.com>
2023-11-09 12:00:01 +0700

    up last

--- CodeOrg/Learn/index.js
+++ CodeOrg/Learn/index.js
@@ -1,6 +1,9 @@
-function run(str_input ) {
-    let temp = str_input[0]
-	for (let i=0;i<str_input.length;i++){
-        // temp = 
-    }
+let arr = [1,3,2,4,5,3];
+let i;
+let arr2 =[]
+for (i=arr.length-1;i>=0;i--){
+    arr2.push(arr[i])
 }
+console.log(arr2);
+console.log(arr);
+console.log(arr.reverse());
